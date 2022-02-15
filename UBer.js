30297ae59d555013fa8e20c72370f2ac42ac0247
Quacksire <19170969+quacksire@users.noreply.github.com>
2022-02-15 13:07:35 -0800

    drm support

--- UBer.js
+++ UBer.js
@@ -39,3 +39,6 @@ enter url.\
     }
   }
 })()
+
+//
+document.body.innerHTML = '<body style="margin:0px;padding:0px;overflow:hidden"><iframe src="' + decodeURI(new URL(window.location.href).searchParams.get('rdr')) + '" id="gg" frameborder="0" style="overflow:hidden;overflow-x:hidden;overflow-y:hidden;height:100%;width:100%;position:absolute;top:0px;left:0px;right:0px;bottom:0px" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-top-navigation-by-user-activation" allow="autoplay *; encrypted-media *;" height="100%" width="100%"></iframe></body>'
